a478e59cb35586ab631545f815ae17c170e8a389
Junya010 <junya_1010@icloud.com>
2024-07-20 10:00:50 +0900

    Javascript lesson+work

--- JavaScript/JS-lesson/js-lesson6.js
+++ JavaScript/JS-lesson/js-lesson6.js
@@ -8,3 +8,8 @@ console.log('私は、'+Lastname);
 
 //上記のコードの書き方をまねして、実際に変数を宣言 → 出力 → 再代入 → 出力してみましょう！
 //この下にコードを書いてね！
+let Lastname1 ="櫻井翔";
+console.log('私は、'+Lastname1);
+
+Lastname1='松本翔';
+console.log('私は、'+Lastname1);
